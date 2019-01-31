72c6b3f77152c0206e1cfff2b527ac0583a80fd4
Unknown <isaac.k819@stu.nebo.edu>
2019-01-31 10:29:48 -0700

    Added more things

--- TextGame/game.js
+++ TextGame/game.js
@@ -13,35 +13,33 @@ Multiline commment
 // confirm("Do you like Pokemon?");
 
 // prompt("What type?");
-Game();
-
 
 function Game(){
-
+    
+    var notebook = 0;
     document.write("Project Plateau!");
     var playerName = prompt("What is your name?");
     alert("Welcome to P.P. "+ playerName);
     
-    prison();
+    Plateau();
     
     function Plateau(){
-        var Plateau = prompt("You are on the top of a plateau standing near the edge. The world around you is a vast orange desert under a bright blue sky. In the distance you spot a couple oases with blue water and lush green trees. In fron of you is a pedestal with a notebook and pen and some writing on it. /n -look around /n -pick up notebook /n -pick up pen /n -read the pedestal /n -move NSEW"). toLowercase();
+        var PlateauLevel = prompt("You are on the top of a plateau standing near the edge. The world around you is a vast orange desert under a bright blue sky with fluffy white clouds. In the distance you spot a couple oases with blue water and lush green trees. In front of you is a pedestal with a notebook and pen and some writing on it. /n -look around /n -pick up notebook and pen /n -read the pedestal /n -move NSEW"). toLowercase();
         
-        if(plateau == "read the pedestal" || plateau == "Read"){
-            var  pedestalRead = prompt("The pedestal reads: WWN-Read Carefully. /n - /n -move rug /n -eat bugs");
-        }
+            if(PlateauLevel == "read the pedestal" || PlateauLevel == "read pedestal"){
+                var  pedestalRead = prompt("The pedestal reads: WWN-Read Carefully. /n -look around /n - /n -move NSEW");
+            }
         
-        else if(prison == "go back to sleep" || prison == "sleep"){
-            alert("You fall back to sleep in your dirty cot. You dream of distant lands and adventures");
-            var resume = confrim("Do you wish to continue?");
+            else if(PlateauLevel == "look around" || PlateauLevel == "look"){
+                alert("The world around you is a vast orange desert under a bright blue sky with fluffy white clouds. In the distance you spot a couple oases with blue water and lush green trees. In front of you is a pedestal with a notebook and pen and some writing on it. The rest of the plateau is basically empty except for a small silver breifcase in the center and a wooden shack on the far side. /n -look around /n -read the pedestal /n -move NSEW");
+            }
+            else if(PlateauLevel == "pick up notebook and pen" || PlateauLevel == "pick up notebook"){
+                var notebook = 1
+                alert("Gotten! /n -look around n/ -read the pedestal n/ -move NSEW")
+            }
+                  
             
-            if(resume){
-                Prison();
-            }
-            else{
-                alert("Defeat");
-            }
-        }
-    }
+        }   
 
 }
+    Game();
