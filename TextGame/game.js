cbcdff10f52b8a90e38e979c35187e6594b9f8a2
Unknown <isaac.k819@stu.nebo.edu>
2019-02-21 10:29:48 -0700

    We all like to have fun

--- TextGame/game.js
+++ TextGame/game.js
@@ -41,44 +41,29 @@ function Game(){
         var PlateauE4 = prompt("You are on the top of a plateau standing near the edge. The world around you is a vast orange desert under a bright blue sky with fluffy white clouds. In the distance you spot a couple oases with blue water and lush green trees. In front of you is a pedestal with a notebook and pen and some writing on it. \n -look around \n -pick up notebook and pen \n -read the pedestal \n -move NSEW").toLowerCase();
         
         if(PlateauE4 == "read the pedestal" || PlateauE4 == "read"){
-            var pedestalRead = prompt("The pedestal reads: WWN-Read Carefully. \n -look around \n -pick up notebook and pen \n -move NSEW").toLowerCase();
+            var pedestalRead = prompt("The pedestal reads: WWN-Read Carefully. \n -look around \n -pick up notebook and pen \n -move NSEW").
             
-            if(PlateauE4 == "look around" || PlateauE4 == "look"){
-            var LookAroundRE = prompt("The world around you is a vast orange desert under a bright blue sky with fluffy white clouds. In the distance you spot a couple oases with blue water and lush green trees. In front of you is a pedestal with a notebook and pen and some writing on it. The rest of the plateau is basically empty except for a small silver breifcase in the center and a wooden shack on the far side. \n -look around \n -read the pedestal \n -move NSEW").toLowerCase();
-            }
-            else if(PlateauE4 == "pick up notebook and pen" || PlateauE4 == "pick up notebook"){
-            var NotebookCollectRE = prompt("Gotten! \n -look around \n -read the pedestal \n -move NSEW").toLowerCase();
-            var notebook = true
-            }
-            else if(PlateauE4 == "move west");
-                PlateauD4();
-            }
-        
+           
         else if(PlateauE4 == "look around" || PlateauE4 == "look"){
-            var LookAround = prompt("The world around you is a vast orange desert under a bright blue sky with fluffy white clouds. In the distance you spot a couple oases with blue water and lush green trees. In front of you is a pedestal with a notebook and pen and some writing on it. The rest of the plateau is basically empty except for a small silver breifcase in the center and a wooden shack on the far side. \n -look around \n -read the pedestal \n -move NSEW").toLowerCase();
+            var LookAround = prompt("The world around you is a vast orange desert under a bright blue sky with fluffy white clouds. In the distance you spot a couple oases with blue water and lush green trees. In front of you is a pedestal with a notebook and pen and some writing on it. The rest of the plateau is basically empty except for a small silver breifcase in the center and a wooden shack on the far side. \n -look around \n -read the pedestal \n -move NSEW").
             }
         
         
         
         else if(PlateauE4 == "pick up notebook and pen" || PlateauE4 == "pick up notebook"){
-            var NotebookCollect = prompt("Gotten! \n -look around \n -read the pedestal \n -move NSEW").toLowerCase();
+            var NotebookCollect = prompt("Gotten! \n -look around \n -read the pedestal \n -move NSEW").
             var notebook = true
             }
         
         
         
         else if(PlateauE4 == "move west");
-            PlateauD4();
+            var PlateauD4 = prompt("Nothing interesting to see here. \n -look around \n -move NSEW").
         
         
         
-        PlateauD4();
-        
-            var PlateauD4 = prompt("Nothing interesting to see here. \n -look around \n -move NSEW").toLowerCase();
             
-                  
-            
-        }   
+            }   
     
     
 }    
